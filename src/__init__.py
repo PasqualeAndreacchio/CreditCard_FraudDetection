2f3d6bd2bca88a0d09927d8baa05d85d54059517
Pasquale Andreacchio <pasquale.andreacchio@studenti.unipd.it>
2026-07-13 21:50:39 +0200

    organization of the code

---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+"""
+Credit Card Fraud Detection Project.
+This project aims to detect fraudulent credit card transactions using deep learning techniques.
+
+Modules:
+    - preprocess.py: Preprocessing utilities for the dataset
+"""
+
+__authors__ = ["Andreacchio Pasquale", 
+               "Calcagni Matteo Renato",
+               "Lavarda Nicola"]
+__email__ = ["pasquale.andreacchio@studenti.unipd.it",
+             "matteorenato.calcagni@studenti.unipd.it",
+             "nicola.lavarda.1@studenti.unipd.it"]
+
